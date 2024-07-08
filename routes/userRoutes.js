5fb00945651f3b7c704e5b7a37684636ef3b53e3
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-08 14:34:52 +0800

    update user route and controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,5 @@ const usersController = require('../controllers/usersController')
 router.route('/')
     .get(usersController.getAllUsers)
     .post(usersController.createNewUser)
-    .patch(usersController.updateUser)
-    .delete(usersController.deleteUser)
-
+    
 module.exports = router
