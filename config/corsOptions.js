f957c9db95cff5c852a485d86a5e2476455baba0
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-08-05 15:43:33 +0800

    fix: cors issue

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,13 @@
+const allowedOrigins = require("./allowedOrigins");
+
 const corsOptions = {
-  origin: "*", // Allow all origins
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };
