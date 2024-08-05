5b81d383e2a41d8591f7ddbdbb23015eff3b8a0e
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-08-05 15:22:54 +0800

    add production on cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://geonx-gis-app.vercel.app/",
+];
 
 module.exports = allowedOrigins;
